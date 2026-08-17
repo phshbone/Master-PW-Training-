49c47ede5aeaee7274df6e22d9f3ff1fbdd10a50
phshbone <phshbone@gmail.com>
2026-08-17 11:40:40 -0400

    Refresh PWA cache for navigation repair

--- service-worker-v1.js
+++ service-worker-v1.js
@@ -1,5 +1,5 @@
-const CACHE='mpw-clean-1.0.3';
-const STATIC=['./','./index.html','./styles-clean.css','./procedures-grid.css','./core.js','./content.js','./modules.js','./app-clean.js','./manifest.webmanifest','./assets/icon-192.png','./assets/icon-512.png'];
+const CACHE='mpw-clean-1.0.4';
+const STATIC=['./','./index.html','./styles-clean.css','./procedures-grid.css','./navigation-repair.css','./core.js','./content.js','./modules.js','./app-clean.js','./manifest.webmanifest','./assets/icon-192.png','./assets/icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
